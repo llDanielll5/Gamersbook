c3522f5d4a207e796da3a9b0ceb8b3a0e655ff41
llDanielll5 <danixmotaa7x@gmail.com>
2021-10-06 03:32:48 -0300

    my facebookClone

---
+++ js/functions.js
@@ -0,0 +1,31 @@
+$(function(){
+    var navUserIcon = $('.nav-user-icon');
+    var darkBtn = document.getElementById('dark-btn');
+
+    navUserIcon.click(function(){
+        $('.settings-menu').slideToggle();
+    })
+
+    darkBtn.onclick = function(){
+        darkBtn.classList.toggle('dark-btn-on');
+        document.body.classList.toggle('dark-theme');
+        
+        if(localStorage.getItem("theme") == "light"){
+            localStorage.setItem("theme", "dark");
+        }else{
+            localStorage.setItem("theme", "light");
+        }
+    }
+
+
+    if(localStorage.getItem("theme") == "light"){
+        darkBtn.classList.remove("dark-btn-on");
+        document.body.classList.remove("dark-theme");
+    }else if(localStorage.getItem("theme") == "dark"){
+        darkBtn.classList.add("dark-btn-on");
+        document.body.classList.add("dark-theme");
+    }else{
+        localStorage.setItem("theme", "light");
+    }
+
+})
